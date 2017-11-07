b47801945217089047a86e8f036e1fa6d439428a
Mr.jie <zjie@360loyo.com>
2017-11-07 11:24:10 +0800

    中文编码

--- routes/build.js
+++ routes/build.js
@@ -15,6 +15,7 @@ router.get('/', function (req, res, next) {
     var des = req.query['des'];//描述
     var user = req.query['user'];//作者
     var type = req.query['type'];//打包 环境和类型
+    des = decodeURI(des);//中文的url解码
     des=des.replace(/\s+/g,"");
     des=des.replace("&","-");
     if (!branch) {
